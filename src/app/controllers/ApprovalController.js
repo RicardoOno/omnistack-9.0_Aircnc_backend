c0b5534bcad8f5a9f6bed2642acd8567ff80ac6d
Ricardo T. Ono <ricardotadashiono@gmail.com>
2019-10-05 20:59:16 -0300

    Fixing some bugs

--- src/app/controllers/ApprovalController.js
+++ src/app/controllers/ApprovalController.js
@@ -3,22 +3,21 @@ const Booking = require('../models/Booking');
 module.exports = {
     async store(req, res){
         const { booking_id } = req.params;
-        console.log(`[approved | booking id] ${booking_id}`);
         const booking = await Booking.findById(booking_id).populate('spot');
-        console.log(`[approved | booking] ${booking}`);
         if(!booking){
             return res.status(401).json({error: 'Not a valid spot'});
         }
         booking.approved = true;
         
-        console.log(`[approved | booking user] ${booking.user}`);
-
         await booking.save();
 
         const bookingUserSocket = req.connectedUsers[booking.user];
-        console.log(req.connectedUsers);
+       
+        console.log(`user socket: ${bookingUserSocket}`);
+
         if(bookingUserSocket) {
-            req.io.to(bookingUserSocket).emit('booking_request', booking);
+            console.log('hello');
+            req.io.to(bookingUserSocket).emit('booking_response', booking);
         }
 
         return res.json(booking);
--- src/app/controllers/RejectionController.js
+++ src/app/controllers/RejectionController.js
@@ -13,9 +13,8 @@ module.exports = {
         await booking.save();
 
         const bookingUserSocket = req.connectedUsers[booking.user];
-
         if(bookingUserSocket) {
-            req.io.to(bookingUserSocket).emit('booking_request', booking);
+            req.io.to(bookingUserSocket).emit('booking_response', booking);
         }
 
 
